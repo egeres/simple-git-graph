6f48d5058236c91f1af48c9b82f0c3be230a0b67
egeres <rafaleto@gmail.com>
2017-08-16 23:45:48 +0100

    created data.json

--- create_json.py
+++ create_json.py
@@ -1,8 +1,22 @@
-import argparse
+import argparse, time, json, os
+# from git import *
 
+#this part handles the arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-d', action="store", dest="d", help='route of the git directory')
 arguments = parser.parse_args()
 
 if not arguments.d:
-    exit()
+    arguments.d = os.getcwd()
+    # exit()
+
+# repo = Repo(arguments.d)
+# assert repo.bare == False
+
+dic_to_write = {}
+
+# for i in repo.iter_commits('master'):
+#     print i
+
+with open('data.json', 'w') as outfile:
+    json.dump(dic_to_write, outfile)
