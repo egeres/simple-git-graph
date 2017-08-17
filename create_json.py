9da5601c05cfabdef6cf2272313b2d79434dbb10
egeres <rafaleto@gmail.com>
2017-08-17 09:40:43 +0100

    script iterates throught commits of the given repo correctly

--- create_json.py
+++ create_json.py
@@ -1,5 +1,5 @@
 import argparse, time, json, os
-# from git import *
+import git
 
 #this part handles the arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
@@ -10,12 +10,36 @@ if not arguments.d:
     arguments.d = os.getcwd()
     # exit()
 
-# repo = Repo(arguments.d)
-# assert repo.bare == False
+repo  = git.Repo(arguments.d)
+tree  = repo.tree()
+paths = [obj.path for obj in tree]
 
-dic_to_write = {}
+latest_commits = {}
+dic_to_write    = {}
 
 # for i in repo.iter_commits('master'):
+#     print i
+
+
+for commit in repo.iter_commits(paths=paths):
+
+    print commit.committed_date
+    print commit.message
+    print commit.author.name
+    print commit.diff()
+
+    # for f in commit.stats.files.keys():
+    #     p = f[:f.index('/')] if '/' in f else f
+    #     if p in latest_commits:
+    #         continue
+    #
+    #     print("adding %s for %s (was: %s)" % (commit, p, f))
+    #     latest_commits[p] = commit
+    #
+    # if len(latest_commits) == len(paths):
+    #     break
+
+# for i in latest_commits.iteritems():
 #     print i
 
 with open('data.json', 'w') as outfile:
