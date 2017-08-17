f45d9cde11325c8c5241b2ff40e42407018f9f85
egeres <rafaleto@gmail.com>
2017-08-17 10:51:45 +0100

    copied an example from bbengfort blog and modified it to get the insertions and deletions

---
+++ bbengfort_approach.py
@@ -0,0 +1,107 @@
+## Imports
+import os
+import git
+
+
+## Module Constants
+DATE_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
+EMPTY_TREE_SHA   = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
+
+
+def versions(path, branch='master'):
+    """
+    This function returns a generator which iterates through all commits of
+    the repository located in the given path for the given branch. It yields
+    file diff information to show a timeseries of file changes.
+    """
+
+    # Create the repository, raises an error if it isn't one.
+    repo = git.Repo(path)
+
+    # Iterate through every commit for the given branch in the repository
+    for commit in repo.iter_commits(branch):
+        # Determine the parent of the commit to diff against.
+        # If no parent, this is the first commit, so use empty tree.
+        # Then create a mapping of path to diff for each file changed.
+        parent = commit.parents[0] if commit.parents else EMPTY_TREE_SHA
+        diffs  = {
+            diff.a_path: diff for diff in commit.diff(parent)
+        }
+
+        # The stats on the commit is a summary of all the changes for this
+        # commit, we'll iterate through it to get the information we need.
+        for objpath, stats in commit.stats.files.items():
+
+            # Select the diff for the path in the stats
+            diff = diffs.get(objpath)
+
+            # If the path is not in the dictionary, it's because it was
+            # renamed, so search through the b_paths for the current name.
+            if not diff:
+                for diff in diffs.values():
+                    if diff.b_path == path and diff.renamed:
+                        break
+
+            # Update the stats with the additional information
+            stats.update({
+                'message': commit.message,
+                'object': os.path.join(path, objpath),
+                'commit': commit.hexsha,
+                'author': commit.author.email,
+                'timestamp': commit.authored_datetime.strftime(DATE_TIME_FORMAT),
+                'time'     : commit.committed_date,
+                'size': diff_size(diff),
+                'type': diff_type(diff),
+            })
+
+            yield stats
+
+
+def diff_size(diff):
+    """
+    Computes the size of the diff by comparing the size of the blobs.
+    """
+    if diff.b_blob is None and diff.deleted_file:
+        # This is a deletion, so return negative the size of the original.
+        return diff.a_blob.size * -1
+
+    if diff.a_blob is None and diff.new_file:
+        # This is a new file, so return the size of the new value.
+        return diff.b_blob.size
+
+    # Otherwise just return the size a-b
+    return diff.a_blob.size - diff.b_blob.size
+
+
+def diff_type(diff):
+    """
+    Determines the type of the diff by looking at the diff flags.
+    """
+    if diff.renamed: return 'R'
+    if diff.deleted_file: return 'D'
+    if diff.new_file: return 'A'
+    return 'M'
+
+data = versions(os.getcwd())
+
+output = []
+used   = []
+
+for i in data:
+    if i["time"] not in used:
+        used.append(i["time"])
+        output.append([i["time"], i["insertions"] , i["deletions"]])
+    else:
+        for n, j in enumerate(output):
+            if j[0] == i["time"]:
+                output[n][1] += i["insertions"]
+                output[n][2] += i["deletions"]
+
+output = sorted(output, key=lambda x: x[0])
+
+print output
+
+
+
+
+#
--- create_json.py
+++ create_json.py
@@ -1,6 +1,9 @@
 import argparse, time, json, os
 import git
 
+
+EMPTY_TREE_SHA   = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
+
 #this part handles the arguments
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('-d', action="store", dest="d", help='route of the git directory')
@@ -21,12 +24,65 @@ dic_to_write    = {}
 #     print i
 
 
+
+def diff_size(diff):
+    """
+    Computes the size of the diff by comparing the size of the blobs.
+    """
+    if diff.b_blob is None and diff.deleted_file:
+        # This is a deletion, so return negative the size of the original.
+        return diff.a_blob.size * -1
+
+    if diff.a_blob is None and diff.new_file:
+        # This is a new file, so return the size of the new value.
+        return diff.b_blob.size
+
+    # Otherwise just return the size a-b
+    return diff.a_blob.size - diff.b_blob.size
+
+def diff_type(diff):
+    """
+    Determines the type of the diff by looking at the diff flags.
+    """
+    if diff.renamed: return 'R'
+    if diff.deleted_file: return 'D'
+    if diff.new_file: return 'A'
+    return 'M'
+
+# for commit in repo.iter_commits(branch):
 for commit in repo.iter_commits(paths=paths):
 
-    print commit.committed_date
-    print commit.message
-    print commit.author.name
-    print commit.diff()
+
+    # parent = commit.parents[0] if commit.parents else EMPTY_TREE_SHA
+    # diffs  = {
+    #     diff.a_path: diff for diff in commit.diff(parent)
+    # }
+    #
+    # for objpath, stats in commit.stats.files.items():
+    #     # Select the diff for the path in the stats
+    #     diff = diffs.get(objpath)
+    #     print 'size', diff_size(diff),
+    #     print 'type', diff_type(diff),
+
+    print "0", commit.committed_date
+    print "0", time.asctime(time.gmtime(commit.committed_date))
+    print "0", time.strftime("%a, %d %b %Y %H:%M", time.gmtime(commit.committed_date))
+    print "1", commit.message
+    print "2", commit.author.name
+    print "3", commit.diff()
+
+    for i in commit.diff():
+        print "5", diff_size(i), "6", diff_type(i)
+
+
+
+    print "\n\n"
+
+
+
+
+
+
 
     # for f in commit.stats.files.keys():
     #     p = f[:f.index('/')] if '/' in f else f
